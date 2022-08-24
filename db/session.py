77449f7ca7b1a2166f6eae9a7b82480118ed5716
jneema <joeyshem33@gmail.com>
2022-08-24 13:11:34 +0300

    small fixess

--- db/session.py
+++ db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 # )Orms's handle to the database is the session and it helps to start talking to our database
 
-engine = create_engine("postgres://upuirxveufqnwh:6e96d1ba907415f41cf9727721662ee58928346a7f1907beaf7af0e7072647d2@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dao165b16hpbgt")
+engine = create_engine("postgresql://upuirxveufqnwh:6e96d1ba907415f41cf9727721662ee58928346a7f1907beaf7af0e7072647d2@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dao165b16hpbgt")
 
 SessionLocal = sessionmaker(bind=engine,
 autocommit=False, autoflush=False)
